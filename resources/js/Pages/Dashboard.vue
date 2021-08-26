559a6335519ebd49fc9c0ee536dc2c56853ff497
designatedcoder <niko@designatedcoder.com>
2021-08-25 21:59:43 -0400

    created Song resource with model, controller, route, seeder, and displayed on dashboard page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,29 +1,92 @@
 <template>
-    <app-layout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
-                </div>
-            </div>
+    <div>
+        <!-- <audio :src="songs[0].src" controls id="audio"></audio> -->
+        <div>
+            <button type="button" @click="prev">Prev</button>
+            <button type="button" @click="pause" v-if="isPlaying">Pause</button>
+            <button type="button" @click="play" v-else>Play</button>
+            <button type="button" @click="next">Next</button>
         </div>
-    </app-layout>
+        <div class="flex flex-col">
+            <button type="button" v-for="(song, index) in songs" :key="index" @click="play(song)">
+                <span>{{ song.title }}</span>
+                <span>{{ song.artist }}</span>
+            </button>
+        </div>
+
+    </div>
+
+
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout.vue'
-    import Welcome from '@/Jetstream/Welcome.vue'
-
     export default {
-        components: {
-            AppLayout,
-            Welcome,
+        data() {
+            return {
+                audio: new Audio(),
+                currentSong: {},
+                index: 0,
+                isPlaying: false,
+                songs: [
+                    {
+                        title: 'Bella Bella Beat',
+                        artist: 'Nana Kwabena',
+                        src: '/storage/audio/Bella Bella Beat.mp3'
+                    },
+                    {
+                        title: 'Breatha',
+                        artist: 'Josh Pan',
+                        src: '/storage/audio/Breatha.mp3'
+                    },
+                    {
+                        title: 'Forever',
+                        artist: 'Anno Domini Beats',
+                        src: '/storage/audio/Forever.mp3'
+                    },
+                    {
+                        title: 'Yah Yah',
+                        artist: 'Josh Pan',
+                        src: '/storage/audio/Yah Yah.mp3'
+                    },
+                ],
+            }
         },
+        mounted() {
+            this.currentSong = this.songs[this.index]
+            this.audio.src = this.currentSong.src
+            console.log(this.audio)
+        },
+        methods: {
+            play(song) {
+                if(typeof song.src != 'undefined') {
+                    this.currentSong = song
+                    this.audio.src = this.currentSong.src
+                }
+                this.audio.play()
+                this.isPlaying = true
+            },
+            pause() {
+                this.audio.pause()
+                this.isPlaying = false
+            },
+            prev() {
+                this.index--
+                if(this.index < 0) {
+                    this.index = this.songs.length - 1
+                }
+                this.currentSong = this.songs[this.index]
+                this.play(this.currentSong)
+            },
+            next() {
+                this.index++
+                if(this.index > this.songs.length - 1) {
+                    this.index = 0
+                }
+                this.currentSong = this.songs[this.index]
+                this.play(this.currentSong)
+            }
+        }
+
     }
 </script>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +14,4 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::get('/', [SongController::class, 'index']);
